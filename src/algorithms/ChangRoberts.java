9ccb747f1eacb50dfc7c93e61afbda69a04e438f
Robert Langtry <rob.langtry2@gmail.com>
2018-05-16 00:34:25 +1000

    Fixed some stuff

--- src/algorithms/ChangRoberts.java
+++ src/algorithms/ChangRoberts.java
@@ -10,17 +10,20 @@ public class ChangRoberts extends AbstractAlgorithm{
         System.out.println("[ \u001B[36mChangRoberts\u001B[0m ] Starting election");
         String[] servers = trader.getServers();
         ConnectionModule connection;
-        int target = Arrays.asList(servers).indexOf(trader.self()) - 1;
-        if(target == -1)
-            target = servers.length - 1;
         try{
-            System.out.println("[ \u001B[36mChangRoberts\u001B[0m ] Sending ring probe to \"" + servers[target] + "\"");
-            connection = new ConnectionModule(servers[target]);
+            System.out.println("[ \u001B[36mChangRoberts\u001B[0m ] Sending ring probe to \"" + this.getLeft(trader) + "\"");
+            connection = new ConnectionModule(this.getLeft(trader));
             connection.send("q" + trader.self()+"~"+trader.self());
             connection.close();
         } catch(IOException e){
-            //
+            System.out.println("[ \u001B[36mChangRoberts\u001B[0m ] Error sending ring probe to \"" + this.getLeft(trader) + "\"");
         }
     }
-
+    public static String getLeft(Trader trader){
+        String[] servers = trader.getServers();
+        int target = Arrays.asList(servers).indexOf(trader.self()) - 1;
+        if(target == -1)
+            target = servers.length - 1;
+        return servers[target];
+    }
 }
--- src/algorithms/EnhancedBully.java
+++ src/algorithms/EnhancedBully.java
@@ -17,15 +17,13 @@ public class EnhancedBully extends AbstractAlgorithm{
                     connection.send("c" + trader.self());
                     connection.close();
                 } catch(IOException e){
+                    System.out.println("[ \u001B[36mEnhancedBully\u001B[0m ] Error contacting \"" + s + "\"");
                     trader.removeServer(s);
                 }
             }
         }
     }
-    public void selectNewHost(Trader trader){
-        System.out.println("[ \u001B[36mEnhancedBully\u001B[0m ] Starting election");
-        int denials = 0;
-        ConnectionModule connection;
+    public ArrayList<String> getCandidates(Trader trader){
         ArrayList<String> candidates = new ArrayList<String>();
         ArrayList<String> ordinary = new ArrayList<String>(Arrays.asList(trader.getServers()));
         String largest;
@@ -40,8 +38,30 @@ public class EnhancedBully extends AbstractAlgorithm{
             ordinary.remove(largest);
             candidates.add(largest);
         }
-        largest = null;
-        for(String s : candidates){
+        return candidates;
+    }
+    public ArrayList<String> getOrdinary(Trader trader){
+        ArrayList<String> candidates = new ArrayList<String>();
+        ArrayList<String> ordinary = new ArrayList<String>(Arrays.asList(trader.getServers()));
+        String largest;
+        while(candidates.size() < ordinary.size()){
+            largest = null;
+            for (String s : ordinary){
+                if (largest == null)
+                    largest = s;
+                else if(EnhancedBully.determine_value(s) > EnhancedBully.determine_value(largest))
+                    largest = s;
+            }
+            ordinary.remove(largest);
+            candidates.add(largest);
+        }
+        return ordinary;
+    }
+    private String lookup(ArrayList<String> list, Trader trader){
+        ConnectionModule connection;
+        String largest = null;
+        int denials = 0;
+        for(String s : list){
             if(EnhancedBully.determine_value(s) > EnhancedBully.determine_value(trader.self())){
                 try{
                     System.out.println("[ \u001B[36mEnhancedBully\u001B[0m ] Sending candidate election notice to \"" + s + "\"");
@@ -59,38 +79,33 @@ public class EnhancedBully extends AbstractAlgorithm{
                 }
             }
         }
-        if(denials == 0){
-            for(String s : ordinary){
-                if(EnhancedBully.determine_value(s) > EnhancedBully.determine_value(trader.self())){
-                    try{
-                        System.out.println("[ \u001B[36mEnhancedBully\u001B[0m ] Sending ordinary election notice to \"" + s + "\"");
-                        connection = new ConnectionModule(s);
-                        connection.send("n" + trader.self());
-                        connection.receive(10000);
-                        if (largest == null)
-                            largest = s;
-                        else if(EnhancedBully.determine_value(s) > EnhancedBully.determine_value(largest))
-                            largest = s;
-                        denials = denials + 1;
-                        System.out.println("[ \u001B[36mEnhancedBully\u001B[0m ] Recieved denial from \"" + s + "\"");
-                    }catch (IOException e){
-                        trader.removeServer(s);
-                    }
-                }
-            }
-        }
-        else {
-            try{
-                connection = new ConnectionModule(largest);
-                connection.send("N" + trader.self());
-                connection.close();
-                System.out.println("[ \u001B[36mEnhancedBully\u001B[0m ] Sending victory notice to \"" + largest + "\"");
-            } catch(IOException e){
-                return;
-            }
+        if(denials == 0)
+            return null;
+        return largest;
+    }
+    public void sendVictory(String host, Trader trader){
+        ConnectionModule connection;
+        try{
+            connection = new ConnectionModule(host);
+            connection.send("N" + trader.self());
+            connection.close();
+            System.out.println("[ \u001B[36mEnhancedBully\u001B[0m ] Sending victory notice to \"" + host + "\"");
+        } catch(IOException e){
+            System.out.println("[ \u001B[36mEnhancedBully\u001B[0m ] Error sending victory notice to \"" + host + "\"");
             return;
         }
-        if(denials == 0 && trader.isLeader() == false){
+    }
+    public void selectNewHost(Trader trader){
+        System.out.println("[ \u001B[36mEnhancedBully\u001B[0m ] Starting election");
+        String host;
+        ConnectionModule connection;
+        ArrayList<String> candidates = getCandidates(trader);
+        ArrayList<String> ordinary = getOrdinary(trader);
+        host = lookup(candidates, trader);
+        if(host == null){
+            host = lookup(ordinary, trader);
+        }
+        if(host == null && !trader.isLeader()){
             try{
                 trader.upgrade();
             } catch (IOException e){
@@ -109,15 +124,8 @@ public class EnhancedBully extends AbstractAlgorithm{
                 }
             }
         }
-        else if(denials>0){
-            try{
-                connection = new ConnectionModule(largest);
-                connection.send("N" + trader.self());
-                connection.close();
-                System.out.println("[ \u001B[36mEnhancedBully\u001B[0m ] Sending victory notice to \"" + largest + "\"");
-            } catch(IOException e){
-                return;
-            }
+        else if(host != null){
+            this.sendVictory(host, trader);
         }
     }
 
