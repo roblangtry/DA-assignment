91fb4cef51cdb1c871883293bea347ea1bdd0115
Robert Langtry <rob.langtry2@gmail.com>
2018-05-18 01:23:34 +1000

    Added null algorithm for failure showing

---
+++ server/algorithms/NullAlgorithm.java
@@ -0,0 +1,11 @@
+package algorithms;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import trader.*;
+
+public class NullAlgorithm extends AbstractAlgorithm{
+    public void selectNewHost(Trader trader){
+        //stub for use when showing issues without leader election
+    }
+}
